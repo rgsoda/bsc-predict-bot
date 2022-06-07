cd276fe044dde8b5e6cf449637e2ba80a515bd65
Rafal Galczynski <rafal.galczynski@cint.com>
2022-06-07 12:10:37 +0200

    fixes

--- strategies/SodaBot.py
+++ strategies/SodaBot.py
@@ -20,10 +20,31 @@ class Bot(BaseBot):
     def get_bet(self, upcoming: Round) -> Optional[Bet]:
 
         timestamp = time.time()
-        msg = "Do akcji jeszcze {} sek, bull:{}, bear:{}"
+        msg = "Do akcji jeszcze {} sek, bull:{}, bear:{} epoch:{}"
         to_action = upcoming.lockTimestamp - timestamp
-        print(msg.format(to_action, upcoming.bullAmount, upcoming.bearAmount)) 
-    
+        print(msg.format(to_action, upcoming.bullAmount, upcoming.bearAmount, upcoming.epoch)) 
+
+
+        incoming_bet = upcoming
+        actual_bet = None
+        last_winner_bet = None
+
+
+        # tutaj pobieraby ostatniego zwyciężce
+        completed = [r for r in self.history if r.oracleCalled]
+        not_completed = [r for r in self.history if not r.oracleCalled]
+        if len(completed) > 0:
+            last_winner_bet = completed[-1]
+
+
+        actual_bet = not_completed[-2]
+
+
+        print("incoming_bet", incoming_bet)
+        print("actual_bet", actual_bet)
+        print("last winner", last_winner_bet)
+
+        return 
         if upcoming.lockTimestamp - timestamp < 10:
             if self.already_bet:
                 return
@@ -35,16 +56,10 @@ class Bot(BaseBot):
                 direction = Direction.BULL
 
 
-            # tutaj pobieraby ostatniego zwyciężce
-            completed = [r for r in self.history if r.oracleCalled]
-            last_winner = None
-            if len(completed) > 0:
-                last_winner = completed[-1]
-
             if self.last_bet: # już postawiliśmy poprzednio, sprawdzamy czy wygraliśmy
                 if self.last_bet.direction == last_winner.winner:
                     print("Stawiamy na {}, ostatnio wygrał {}, stawka {}".format(direction, last_winner.winner, self.bet_size_eth * self.multiplier))
-                    self.last_bet = Bet(direction=direction, amount_eth=self.bet_size_eth * self.multiplier, epoch=upcoming.epoch)
+                   #self.last_bet = Bet(direction=direction, amount_eth=self.bet_size_eth * self.multiplier, epoch=upcoming.epoch)
                     self.already_bet = True
                     self.multiplier = 1
                 else:
@@ -53,7 +68,8 @@ class Bot(BaseBot):
                     self.multiplier *= 2
                     return
             else: # pierwse postawienie beta, na cokolwiek
-                self.last_bet = Bet(direction=direction, amount_eth=self.bet_size_eth, epoch=upcoming.epoch)
+                #self.last_bet = Bet(direction=direction, amount_eth=self.bet_size_eth, epoch=upcoming.epoch)
+                pass
 
         else:
             self.already_bet = False 
